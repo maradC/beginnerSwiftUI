3b8de8a29a59fc29d426d7636880a81a8070828f
maradC <maradiagachristopher5@gmail.com>
2023-11-12 14:04:07 -0500

    I added labels,images, and reorganized

--- beginnerSwift/ViewController.swift
+++ beginnerSwift/ViewController.swift
@@ -11,16 +11,20 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var changeBackgroundColor: UIButton!
     
+    @IBOutlet weak var movingWord: UILabel!
+   
+    @IBOutlet weak var searchBar: UISearchBar!
+        
+    @IBAction func changeColorButtonTapped(_ sender: UIButton) {
+        let randomColor = changeColor()
+                view.backgroundColor = randomColor
+    }
+    
     func changeColor() -> UIColor{
         let red = CGFloat.random(in: 0...1)
         let green = CGFloat.random(in: 0...1)
         let blue = CGFloat.random(in: 0...1)
         return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
-    }
-    
-    @IBAction func changeColorButtonTapped(_ sender: UIButton) {
-        let randomColor = changeColor()
-                view.backgroundColor = randomColor
     }
    
     override func viewDidLoad() {
