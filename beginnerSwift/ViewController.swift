5cf2a069cc5b1e9241e1572c72e6e6e4cf045ebb
maradC <maradiagachristopher5@gmail.com>
2023-11-12 09:50:14 -0500

    I add the basic logic to be able change the background color.

--- beginnerSwift/ViewController.swift
+++ beginnerSwift/ViewController.swift
@@ -9,6 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var changeBackgroundColor: UIButton!
+    func changeColor() -> UIColor{
+        let red = CGFloat.random(in: 0...1)
+        let green = CGFloat.random(in: 0...1)
+        let blue = CGFloat.random(in: 0...1)
+            
+        return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
