d16a9313cae27b1942a26275e11f40aa4e2880f9
maradC <maradiagachristopher5@gmail.com>
2023-11-12 13:29:05 -0500

    Added Feature that will change color when tapped

--- beginnerSwift/ViewController.swift
+++ beginnerSwift/ViewController.swift
@@ -10,14 +10,19 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var changeBackgroundColor: UIButton!
+    
     func changeColor() -> UIColor{
         let red = CGFloat.random(in: 0...1)
         let green = CGFloat.random(in: 0...1)
         let blue = CGFloat.random(in: 0...1)
-            
         return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
     }
     
+    @IBAction func changeColorButtonTapped(_ sender: UIButton) {
+        let randomColor = changeColor()
+                view.backgroundColor = randomColor
+    }
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
